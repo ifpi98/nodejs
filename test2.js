b2214981842d7897dda76a7e363b61d211b987c7
TQ2L774\학생 <abc@a.com>
2023-06-08 14:11:43 +0900

    오전 수업 내용 업데이트

---
+++ test2.js
@@ -0,0 +1,12 @@
+// var request = require("request");
+// var url = 'http://www.daum.net';
+
+// request(url,(error,Response,body) => {
+//     // console.log(body);
+//     console.log(error);
+// });
+
+var abc = [{age:1},2,3,4];
+console.log(abc);
+console.log(JSON.stringify(abc));
+
