2d66fb1e723b65cdc937ac924a35efb15a5b97a5
TQ2L774\학생 <abc@a.com>
2023-06-21 13:06:54 +0900

    0621 오전 수업 내용 업데이트

---
+++ students.js
@@ -0,0 +1,137 @@
+// 모듈 추출한다.
+var http = require('http');
+var express = require('express');
+var mysql = require('mysql');
+
+// 데이터베이스와 연결
+var client = mysql.createConnection({
+    user: 'root',
+    password: '1234',
+    database: 'college'
+});
+
+// 웹서버를 생성한다.
+var app = express();
+app.use(express.static('public'));  // 기본 폴더 지정
+// post요청시 파라미터 전달
+app.use(express.bodyParser());
+app.use(app.router);
+
+//라우터 설정
+app.get('/students', function (req, res) {
+    var sql = 'select * from students';
+    client.query(sql, function (err, result) {
+        // console.log(result);
+        res.send(result);
+    });
+});
+
+// 개별 조회
+app.get('/students/:id', function (req, res) {
+    // var id = Number(req.param('id'));
+    // // var query = 'select * from products where id=' + id;
+    // var query = 'select * from products where id=?';
+    // client.query(query,[id], function (err, result) {
+    //     if(result==""){
+    //         res.send("없는 데이터입니다.");
+    //     } else{
+    //     // console.log(result);
+    //     res.send(result);
+    //     }
+    // });
+
+});
+
+// 데이터 추가, 회원 가입
+app.post('/students', function (req, res) {
+    var id = req.body.id;
+    var name = req.body.name;
+    var password = req.body.password;
+    var email = req.body.email;
+    var gender = req.body.gender;
+    var data = {id, name, password, email, gender}
+    // console.log(data);
+    
+    var sql = "insert into students (id, name, password, email, gender) values (?,?,?,?,?)";
+    client.query(sql,[id, name, password, email, gender], function (err, result) {
+        // console.log(result);
+        res.redirect("member_ok.html");
+        // if(err){
+        //     console.log("error: ", err.sqlMessage);
+        // }
+        // if(result){
+        //     res.redirect("member_ok.html");
+        // }else{
+        //     res.send(err || result);
+        // }
+        
+    });
+
+});
+app.put('/students/:id', function (req, res) { 
+    // var id = req.params.id;
+    // var name = req.body.name;
+    // var modelnumber = req.body.modelnumber;
+    // var series = req.body.series;
+    // var query = "update products set name=?, modelnumber=?, series=? where id=?";
+    // console.log(query);
+    // client.query(query,[name, modelnumber, series, id], function (err, result) {
+    //     // console.log(result);
+    //     res.send(result);
+    // });
+
+});
+app.del('/students/:id', function (req, res) { 
+    var id = req.params.id;
+    // console.log(id);
+    var sql = "DELETE FROM students WHERE id=?";
+    // // console.log(query);
+    client.query(sql,[id], function (err, data) {
+    // console.log(result);
+        // res.send(data);
+    // if (err) throw err;
+        console.log(data);
+        if(data.affectedRows == 0){
+            res.send("없는 데이터입니다");
+        } else {
+            res.send("id: " + id + "를 삭제했습니다.")
+        }
+    });
+
+});
+
+//parameter
+app.all ('/parameter', function(req,res){
+    // var name = req.body.name || req.query.name;
+    // var modelnumber = req.body.modelnumber || req.query.modelnumber;
+    // var series = req.body.series || req.query.series;
+
+    // var data = {name, modelnumber, series};
+    // var sql = "insert into products(name, modelnumber, series) values (?,?,?)";
+
+    // client.query(sql, [name, modelnumber, series], function(err, result){
+    //     // console.log(data);
+    //     if(result){
+    //         res.send(data);
+    //     } else {
+    //         res.send(err)
+    //     }
+    // })
+
+})
+
+// http://127.0.0.1:52273/parameter?name=park&modelnumber=333&series=student
+
+
+// 웹서버를 실행한다.
+http.createServer(app).listen(52273, function () {
+    console.log("Server Running at ..");
+    console.log("http://127.0.0.1:52273");
+    console.log("http://127.0.0.1:52273/member.html");
+    
+})
+
+// var query = 'select * from products';
+// client.query(query, function (err, result) {
+//         console.log(result);
+// });
