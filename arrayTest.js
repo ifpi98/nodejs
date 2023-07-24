bd34b422703076ecde69dd225b0719910d95fbd8
TQ2L774\학생 <abc@a.com>
2023-07-24 09:37:47 +0900

    지난 파일 폴더 위치 변경

---
+++ arrayTest.js
@@ -0,0 +1,15 @@
+var plots = [20,5,8,15,20];
+var star = "*"
+console.log("var plots = [20,5,8,15,20]");
+
+for (var i = 0; i<plots.length ; i++){
+    console.log(plots[i] * star);
+
+    // for (var j = 0; j < plots[i]; j++){
+    //     console.log("*")
+    // }
+    // console.log(plots[i]);
+
+}
+
+
